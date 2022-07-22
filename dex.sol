60675c3532c4ea83f6d84fe39d776f51f81ac2bf
Ricky Cheng <rickycheng@genesisblockhk.com>
2022-07-22 22:49:42 +0800

    initial commit

---
+++ dex.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.6.3;
+
+contract Dex {
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    // bytes32 is key type
+    // Token is value type
+    // tokens is name of the array
+    mapping(bytes32 => Token) public tokens;
+
+    // bytes32 list
+    bytes32[] public tokenlist;
+    address public admin;
+
+    constructor() public {
+        admin = msg.sender;
+    }
+
+    // modifiers
+    modifier onlyAdmin() {
+        require(msg.sender == admin, 'only admin');
+        _;
+    }
+
+    function addToken(bytes32 ticker, address tokenAddress) onlyAdmin() external {
+
+    }
+    
+    
+}
