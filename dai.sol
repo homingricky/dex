8923858d22db6c784f2efbc4522dfdad14d6e838
Ricky <96760876+homingricky@users.noreply.github.com>
2022-07-23 22:11:24 +0800

    feat: createLimitOrder()

--- dai.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.6.3;
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/Creepybits/openzeppelin/blob/main/contracts/token/ERC20/ERC20Detailed.sol";
-
-contract Dai is ERC20, ERC20Detailed {
-    constructor() ERC20Detailed('DAI', 'Dai Stablecoin', 18) public {}
-}
